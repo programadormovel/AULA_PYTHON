268400f07d19990d13b37db766eec9ee43eda238
adrianodomingues <adrianodomingues@hotmail.com>
2025-04-22 13:31:52 -0300

    gravando dados em um dicionário

--- gravar_arquivo.py
+++ gravar_arquivo.py
@@ -8,3 +8,16 @@ def gravar_arquivo(arquivo, dados):
     with open("arquivo.json", "w", encoding="utf-8") as arquivo:
         arquivo.write(json_str)
         arquivo.close()
+
+def gravar_arquivo_teclado(arquivo, dados_adicionais):
+    dados = {}
+
+    with open("arquivo.json", "r", encoding="utf-8") as arquivo:
+        dados = json.load(arquivo)
+        arquivo.close()
+
+    with open("arquivo.json", "w", encoding="utf-8") as arquivo:
+        dados.update(dados_adicionais)
+        json_str = json.dumps(dados)
+        arquivo.write(json_str)
+        arquivo.close()
--- np1.py
+++ np1.py
@@ -1,14 +1,16 @@
 from gravar_arquivo import gravar_arquivo
+from gravar_arquivo import gravar_arquivo_teclado
 from ler_arquivo import ler_arquivo
 from ler_arquivo import ler_arquivo_desestruturado
 
 def menu():
     print("=== Menu de Opções ===")
-    print("1. Opção 1")
-    print("2. Opção 2")
-    print("3. Opção 3")
-    print("4. Sair")
-    escolha = input("Escolha uma opção (1-4): ")
+    print("1. Criação e inicialização do arquivo")
+    print("2. Leitura do arquivo gravado")
+    print("3. Leitura dos dados desestruturados")
+    print("4. Gravar dados do teclado no arquivo")
+    print("5. Sair")
+    escolha = input("Escolha uma opção (1-5): ")
 
     return escolha
 
@@ -18,13 +20,20 @@ def opcao_1():
 
 def opcao_2():
     print("Lendo arquivo e apresentando na tela.")
-    dados = ler_arquivo("arquivo.json")
-    print(dados)
+    dados_arquivo = ler_arquivo("arquivo.json")
+    print(dados_arquivo)
 
 def opcao_3():
     print("Lendo arquivo com dados desestruturados.")
-    dados = ler_arquivo_desestruturado("arquivo.json")
-    print(dados['title'])
+    dados_arquivo = ler_arquivo_desestruturado("arquivo.json")
+    print(dados_arquivo['title'])
+    if 'dados' in dados_arquivo:3
+        print(dados_arquivo['dados'])
+
+def opcao_4():
+    dados_teclado = input("Digite algum texto para ser gravado no arquivo: ")
+    print("Gravando arquivo com dados em formato JSON...")
+    gravar_arquivo_teclado("arquivo.json", {"dados": dados_teclado})
 
 def main():
     while True:
@@ -37,6 +46,8 @@ def main():
         elif escolha == "3":
             opcao_3()
         elif escolha == "4":
+            opcao_4()
+        elif escolha == "5":
             print("Saindo do programa...")
             break
         else:
